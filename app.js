44106b2a0c3b11402e5841c07102842ad7c5f13d
paulowarren31 <paulowarren31@gmail.com>
2015-02-21 13:48:19 -0500

    nothing changed really

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var express  = require('express');
 var cors = require('cors');
+var mongoose = require('mongoose')
 var app = express();
+var database = mongoose.connection;
+
+mongoose.connect('mongodb://paulo:Pinzon123@proximus.modulusmongo.net:27017/juHa7gog');
+//setting up the database
+
 
 var globalScore=1;
 
 app.use(cors());
+
+
+app.get('file:///home/paulo/Github/LeagueQuiz/index.html#login')
 
 app.get('/api/globalscore',function(req,res){
   res.send({score:globalScore});
@@ -13,8 +22,27 @@ app.put('/api/globalscore/:num',function(req,res){
 
   globalScore=globalScore+parseInt(req.params.num);
 
-  res.send({sent:parseInt(req.params.num),type:typeof parseInt(req.params.num)})
+  res.send({sent:parseInt(req.params.num)})
 })
+app.put('/api/globalscore/:username/:password',function(req,res){
+	Account.create({
+		username: req.params.username,
+		password: req.params.password,
+		rank: 0	
+	},function(err, acc){
+		Account.find(function(err, accounts){
+			res.send(accounts);
+		})
+	})
+
+});
+
+var Account = mongoose.model('Account',{
+	username: String,
+	password: String,
+	rank: Number
+});
+
 
 app.listen(8080);
 console.log("App listening on port 8080");
